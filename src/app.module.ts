9d9445b3121b5114c5d71cde979cf64e22e1f9a6
duypv <vanduyptit@gmail.com>
2024-02-25 11:04:35 +0700

    Microservice Add code generate resource nest g resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { RestapiModule } from './restapi/restapi.module';
 import { GraphqlCodefirstModule } from './graphql-codefirst/graphql-codefirst.module';
 import { GraphqlSchemafirstModule } from './graphql-schemafirst/graphql-schemafirst.module';
+import { MicroserviceModule } from './microservice/microservice.module';
 
 @Module({
-  imports: [RestapiModule, GraphqlCodefirstModule, GraphqlSchemafirstModule],
+  imports: [RestapiModule, GraphqlCodefirstModule, GraphqlSchemafirstModule, MicroserviceModule],
   controllers: [AppController],
   providers: [AppService],
 })
